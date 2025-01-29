1755b1ca7d3b1a46530c0618f6b9506020f63932
Dimitar Mitev <d.mitev920123@gmail.com>
2025-01-29 17:54:58 +0200

    feat: additional links into the sign-in and sign-up forms

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -16,6 +16,7 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
+import Link from "next/link";
 
 const formSchema = z.object({
   email: z.string().trim().email(),
@@ -113,6 +114,17 @@ export const SignInCard = () => {
           <FaGithub className="mr-2 size-5" />
           Login with GitHub
         </Button>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex items-center justify-center">
+        <p>
+          Don&apos;t have an account?{" "}
+          <Link href="/sign-up">
+            <span className="text-blue-700 hover:cursor-pointer">Sign Up</span>
+          </Link>
+        </p>
       </CardContent>
     </Card>
   );
