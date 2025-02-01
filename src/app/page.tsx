86388b2819cd968845ce92a399fa415a1f3fbe34
Dimitar Mitev <d.mitev920123@gmail.com>
2025-02-01 17:51:26 +0200

    feat: logout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import { useCurrent } from "@/features/auth/api/use-current";
 import { useLogout } from "@/features/auth/api/use-logout";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
   const router = useRouter();
@@ -19,8 +20,7 @@ export default function Home() {
   }, [data]);
   return (
     <div className="">
-      Only visible for authorized users
-      <Button onClick={() => mutate()}>Logout</Button>
+      <UserButton />
     </div>
   );
 }
