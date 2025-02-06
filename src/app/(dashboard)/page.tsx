79db2df9b1478bda3b63f1850d10c0f622381c5e
Dimitar Mitev <d.mitev920123@gmail.com>
2025-02-06 11:30:13 +0200

    feat: redirect to selected workspace id

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 import { getCurrent } from "@/features/auth/actions";
-import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
+import { getWorkspaces } from "@/features/workspaces/actions";
 
 export default async function Home() {
   const user = await getCurrent();
@@ -8,9 +8,10 @@ export default async function Home() {
     redirect("/sign-in");
   }
 
-  return (
-    <div className="bg-neutral-500 p-4 h-full">
-      <CreateWorkspaceForm />
-    </div>
-  );
+  const workspaces = await getWorkspaces();
+  if (workspaces?.total === 0) {
+    redirect("/workspaces/create");
+  } else {
+    redirect(`/workspaces/${workspaces?.documents[0].$id}`);
+  }
 }
---
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -0,0 +1,5 @@
+const WorkspaceIdPage = () => {
+  return <div>Workspace id</div>;
+};
+
+export default WorkspaceIdPage;
