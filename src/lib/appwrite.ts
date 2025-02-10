8c3ffe7b1090765c770d68fd53ac5bb766105f58
Dimitar Mitev <d.mitev920123@gmail.com>
2025-02-10 18:26:53 +0200

    feat: implement members management functionality

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,5 +1,5 @@
 import "server-only";
-import { Client, Account, Storage, Users, Databases } from "node-appwrite";
+import { Client, Account, Users, Databases } from "node-appwrite";
 import { cookies } from "next/headers";
 import { AUTH_COOKIE } from "@/features/auth/constants";
 
@@ -35,6 +35,9 @@ export async function createAdminClient() {
   return {
     get account() {
       return new Account(client);
+    },
+    get users() {
+      return new Users(client);
     },
   };
 }
