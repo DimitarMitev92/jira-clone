8b2f902179e16b6d63cf997834e348cfd12b1888
Dimitar Mitev <d.mitev920123@gmail.com>
2025-02-20 13:57:33 +0200

    feat: update task creation form with due date and assignee fields, and upgrade date

--- src/features/tasks/components/create-task-form.tsx
+++ src/features/tasks/components/create-task-form.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Image from "next/image";
 import { z } from "zod";
-import { useRef } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -17,63 +15,51 @@ import {
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { ImageIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useCreateTask } from "../api/use-create-task";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { useRouter } from "next/navigation";
 import { createTaskSchema } from "../schemas";
+import { DatePicker } from "@/components/date=picker";
 
 interface CreateTaskFormProps {
   onCancel?: () => void;
-  projectOptions: { id: string }[];
+  projectOptions: { id: string; name: string; imageUrl: string }[];
+  memberOptions: { id: string; name: string }[];
 }
 
-export const CreateTaskForm = ({ onCancel }: CreateTaskFormProps) => {
+export const CreateTaskForm = ({
+  onCancel,
+  projectOptions,
+  memberOptions,
+}: CreateTaskFormProps) => {
   const router = useRouter();
   const workspaceId = useWorkspaceId();
   const { mutate, isPending } = useCreateTask();
 
-  const inputRef = useRef<HTMLInputElement>(null);
-
   const form = useForm<z.infer<typeof createTaskSchema>>({
     resolver: zodResolver(createTaskSchema.omit({ workspaceId: true })),
     defaultValues: {
-      name: "",
+      workspaceId,
     },
   });
 
   const onSubmit = (values: z.infer<typeof createTaskSchema>) => {
-    const finalValues = {
-      ...values,
-      workspaceId,
-      image: values.image instanceof File ? values.image : "",
-    };
     mutate(
-      { form: finalValues },
+      { json: { ...values, workspaceId } },
       {
         onSuccess: ({ data }) => {
           form.reset();
-          router.push(`/workspaces/${workspaceId}/projects/${data.$id}`);
+          // TODO: Redirect to the task page
         },
       }
     );
   };
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      form.setValue("image", file);
-    }
-  };
-
   return (
     <Card className="w-full h-full border-none shadow-none">
       <CardHeader className="flex p-7">
-        <CardTitle className="text-xl font-bold">
-          Create a new project
-        </CardTitle>
+        <CardTitle className="text-xl font-bold">Create a new task</CardTitle>
       </CardHeader>
       <div className="px-7">
         <DottedSeparator />
@@ -86,9 +72,9 @@ export const CreateTaskForm = ({ onCancel }: CreateTaskFormProps) => {
                   name="name"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>Project Name</FormLabel>
+                      <FormLabel>Task Name</FormLabel>
                       <FormControl>
-                        <Input {...field} placeholder="Enter workspace name" />
+                        <Input {...field} placeholder="Enter task name" />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -96,74 +82,26 @@ export const CreateTaskForm = ({ onCancel }: CreateTaskFormProps) => {
                 />
                 <FormField
                   control={form.control}
-                  name="image"
+                  name="dueDate"
                   render={({ field }) => (
-                    <div className="flex flex-col gap-y-2">
-                      <div className="flex items-center gap-x-5">
-                        {field.value ? (
-                          <div className="size-[72px] relative rounded-md overflow-hidden">
-                            <Image
-                              src={
-                                field.value instanceof File
-                                  ? URL.createObjectURL(field.value)
-                                  : field.value
-                              }
-                              alt="Workspace Image"
-                              fill
-                              className="object-cover"
-                            />
-                          </div>
-                        ) : (
-                          <Avatar className="size-[72px]">
-                            <AvatarFallback>
-                              <ImageIcon className="size-[36px] text-neutral-400" />
-                            </AvatarFallback>
-                          </Avatar>
-                        )}
-                        <div className="flex flex-col">
-                          <p className="text-sm">Project Icon</p>
-                          <p className="text-sm text-muted-foreground">
-                            JPG, PNG, SVG or JPEG, max 1mb
-                          </p>
-                          <input
-                            className="hidden"
-                            type="file"
-                            accept=".jpg, .png, .svg, .jpeg"
-                            ref={inputRef}
-                            onChange={handleImageChange}
-                            disabled={isPending}
-                          />
-                          {field.value ? (
-                            <Button
-                              type="button"
-                              variant="destructive"
-                              size="xs"
-                              disabled={isPending}
-                              className="w-fit mt-2"
-                              onClick={() => {
-                                field.onChange(null);
-                                if (inputRef.current) {
-                                  inputRef.current.value = "";
-                                }
-                              }}
-                            >
-                              Remove Image
-                            </Button>
-                          ) : (
-                            <Button
-                              type="button"
-                              variant="teritary"
-                              size="xs"
-                              disabled={isPending}
-                              className="w-fit mt-2"
-                              onClick={() => inputRef.current?.click()}
-                            >
-                              Upload Image
-                            </Button>
-                          )}
-                        </div>
-                      </div>
-                    </div>
+                    <FormItem>
+                      <FormLabel>Due Date</FormLabel>
+                      <FormControl>
+                        <DatePicker {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="assigneeId"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Assignee</FormLabel>
+                      <FormControl></FormControl>
+                      <FormMessage />
+                    </FormItem>
                   )}
                 />
               </div>
@@ -180,7 +118,7 @@ export const CreateTaskForm = ({ onCancel }: CreateTaskFormProps) => {
                   Cancel
                 </Button>
                 <Button type="submit" size="lg" disabled={isPending}>
-                  Create Project
+                  Create Task
                 </Button>
               </div>
             </form>
