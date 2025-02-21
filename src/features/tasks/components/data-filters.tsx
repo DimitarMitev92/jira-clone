40d9a24cc4ff8d3c17dce3af1bdfb5e1a5eef3ea
Dimitar Mitev <d.mitev920123@gmail.com>
2025-02-21 13:01:07 +0200

    feat: implement custom hook for task filters and integrate with data filters component

--- src/features/tasks/components/data-filters.tsx
+++ src/features/tasks/components/data-filters.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/select";
 import { ListCheckIcon } from "lucide-react";
 import { TaskStatus } from "../types";
+import { useTaskFilters } from "../hooks/use-task-filters";
 
 interface DataFiltersProps {
   hideProjectFilter?: boolean;
@@ -39,11 +40,21 @@ export const DataFilters = ({ hideProjectFilter }: DataFiltersProps) => {
     label: member.name,
   }));
 
+  const [{ status, assigneeId, projectId, dueDate }, setFilters] =
+    useTaskFilters();
+
+  const onStatusChange = (value: string) => {
+    setFilters({ status: value === "all" ? null : (value as TaskStatus) });
+  };
+
   if (isLoading) return null;
 
   return (
     <div className="flex flex-col lg:flex gap-2">
-      <Select defaultValue={undefined} onValueChange={() => {}}>
+      <Select
+        defaultValue={status ?? undefined}
+        onValueChange={(value) => onStatusChange(value)}
+      >
         <SelectTrigger className="w-full lg:w-auto h-8">
           <div className="flex items-center pr-2">
             <ListCheckIcon className="size-4 mr-2" />
