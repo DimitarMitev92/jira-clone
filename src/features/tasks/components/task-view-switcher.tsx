53251ec989ed6e000111f135909c0ecb6aac80ca
Dimitar Mitev <d.mitev920123@gmail.com>
2025-02-25 16:36:57 +0200

    feat: add Kanban view for task management with drag-and-drop functionality

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -11,6 +11,7 @@ import { DataFilters } from "./data-filters";
 import { useTaskFilters } from "../hooks/use-task-filters";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
+import { DataKanban } from "./data-kanban";
 
 export const TaskViewSwitcher = () => {
   const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters();
@@ -63,7 +64,7 @@ export const TaskViewSwitcher = () => {
               <DataTable columns={columns} data={tasks?.documents ?? []} />
             </TabsContent>
             <TabsContent value="kanban" className="mt-0">
-              Data kanban
+              <DataKanban data={tasks?.documents || []} />
             </TabsContent>
             <TabsContent value="calendar" className="mt-0">
               Data calendar
