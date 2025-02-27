2b4c0acf9a27f147aae2a111b27253675bf9e179
Dimitar Mitev <d.mitev920123@gmail.com>
2025-02-27 12:14:17 +0200

    feat: building a data calendar

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -15,6 +15,7 @@ import { DataKanban } from "./data-kanban";
 import { useCallback } from "react";
 import { TaskStatus } from "../types";
 import { useBulkUpdateTasks } from "../api/use-bulk-update-tasks";
+import { DataCalendar } from "./data-calendar";
 
 export const TaskViewSwitcher = () => {
   const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters();
@@ -77,12 +78,12 @@ export const TaskViewSwitcher = () => {
             </TabsContent>
             <TabsContent value="kanban" className="mt-0">
               <DataKanban
-                data={tasks?.documents || []}
+                data={tasks?.documents ?? []}
                 onChange={onKanbanChange}
               />
             </TabsContent>
-            <TabsContent value="calendar" className="mt-0">
-              Data calendar
+            <TabsContent value="calendar" className="mt-0 h-full pb-4">
+              <DataCalendar data={tasks?.documents ?? []} />
             </TabsContent>
           </>
         )}
