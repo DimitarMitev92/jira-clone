c5baf978eb1a452e42b80e45f19ce99a0c3591ed
Dimitar Mitev <d.mitev920123@gmail.com>
2025-03-07 12:30:02 +0200

    fix: remove unused properties

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -2,13 +2,10 @@
 
 import { createAdminClient } from "@/lib/appwrite";
 import { redirect } from "next/navigation";
-import { headers } from "next/headers";
 import { OAuthProvider } from "node-appwrite";
 
 export async function signUpWithGithub() {
   const { account } = await createAdminClient();
-
-  // const origin = headers().get("origin");
 
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Github,
